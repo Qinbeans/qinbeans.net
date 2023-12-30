0dc3f08b90893a4bfa6fa20c1cffef45839bc072
Ryan Fong <rybeats@live.com>
2023-12-30 10:43:52 -0800

    Starting anew with sveltekit

--- frontend/src/components/views/loading.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import BaoziDance from "../../assets/baozidance.webp";
-</script>
-
-<div class="w-screen h-screen grid place-content-center">
-    <img src={BaoziDance} height="100" alt="baozi">
-    <div class="text-pink-500 text-2xl underline">Loading</div>
-</div>
