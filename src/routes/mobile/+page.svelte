2f86d1540f225adbb0135f454a6b49a838a1dc25
Ryan Fong <rybeats@live.com>
2024-01-01 16:03:14 -0800

    Added some components for cards

---
+++ src/routes/mobile/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import Card from '$lib/components/card.svelte';
+    const date = new Date().toLocaleDateString();
+</script>
+
+<div class="grid grid-cols-1 gap-3 grid-flow-row w-dvw h-full pt-14 px-1 holder overflow-scroll scroll-smooth">
+    <!--  -->
+</div>
