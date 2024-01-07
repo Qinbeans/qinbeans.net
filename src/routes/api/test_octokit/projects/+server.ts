8bfd8643ebe9a9994f473e893f558f5e903aeb26
Ryan Fong <rybeats@live.com>
2024-01-06 18:15:37 -0800

    trying to figure out where cloudflare is messing up

--- src/routes/api/test_octokit/projects/+server.ts
+++ src/routes/api/test_octokit/projects/+server.ts
@@ -1,8 +1,19 @@
-import { octokit } from "$lib/scripts/octokit";
+import { App } from "octokit";
+import { PUBLIC_OCTOKIT, PUBLIC_OCTOKEY, PUBLIC_OCTOIID } from "$env/static/public"
 import { json } from '@sveltejs/kit';
 
 /** @type {import('./$types').RequestHandler} */
 export const GET = async ({ request }) => {
+    
+    const envToKey = (env: string) => {
+        return env.replaceAll("<nl>", "\n");
+    }
+    
+    const app = new App({
+        appId: PUBLIC_OCTOKIT,
+        privateKey: envToKey(PUBLIC_OCTOKEY),
+    });
+    const octokit = await app.getInstallationOctokit(parseInt(PUBLIC_OCTOIID,10));
     await octokit.request('GET /users/{username}/repos', {
         username: 'Qinbeans',
         headers: {
