e9db7964e5d733b77489b0eaf048f763dc9361f3
Ryan Fong <rybeats@live.com>
2024-02-16 17:33:09 -0800

    Attempt 2 trying to fix scaling

--- src/routes/mobile/awards/+page.svelte
+++ src/routes/mobile/awards/+page.svelte
@@ -8,23 +8,6 @@
     export let data: any;
 
     const awards: Award[] = data.awards;
-    let iframes: Array<HTMLIFrameElement> = new Array(awards.length);
-
-    const responsiveIframe = (evt: Event) => {
-        const iframe = evt.target as HTMLIFrameElement;
-        if (iframe) {
-            const height = iframe.contentWindow?.document.body.scrollHeight;
-            iframe.style.height = `${height}px`;
-        }
-    };
-
-    onMount(() => {
-        iframes.forEach((iframe) => {
-            if (iframe) {
-                iframe.addEventListener("load", responsiveIframe);
-            }
-        });
-    });
 </script>
 
 <div class="grid grid-cols-1 gap-3 grid-flow-row w-dvw h-full py-14 px-1 holder overflow-scroll scroll-smooth">
@@ -34,7 +17,7 @@
             author={award.from}
             date={new Date(award.date).toLocaleDateString()}
             title_class="text-3xl truncate"
-            inner_padding="px-[5%] py-1 h-full"
+            inner_padding="px-[5%] py-1 h-[80%]"
             background="bg-black/25"
             color="text-white"
             width="w-fill"
@@ -42,7 +25,7 @@
             enable_image={false}
         >
             {#if award.data[0] === "embed"}
-                <iframe bind:this={iframes[i]} title={award.title} src={award.data[1]} height="80%" width="100%" class="overflow-hidden" frameborder="0"/>
+                <iframe title={award.title} src={award.data[1]} class="overflow-hidden w-full h-full" frameborder="0"/>
             {:else}
                 {award.data[1]}
             {/if}
