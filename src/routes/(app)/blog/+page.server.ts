b36a81e43a537eba3c71fd237ea7d73eb7155358
Ryan Fong <rybeats@live.com>
2024-03-02 15:45:34 -0800

    First attempt at revising Blog loading

--- src/routes/(app)/blog/+page.server.ts
+++ src/routes/(app)/blog/+page.server.ts
@@ -2,13 +2,10 @@ import { blogs } from "$lib/scripts/store";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({url}) => {
-    // grab from "blogs" table in order of newest to oldest and limit to 5
-    const page = url.searchParams.get('page')?parseInt(url.searchParams.get('page')!,10):1
     const per_page = url.searchParams.get('per_page')?parseInt(url.searchParams.get('per_page')!,10):6
     const origin = url.origin
     return {
         origin,
-        page,
         per_page
     }
 };
--- src/routes/api/blog/+server.ts
+++ src/routes/api/blog/+server.ts
@@ -6,6 +6,8 @@ export const GET: RequestHandler = async ({url}) => {
     // grab from "blogs" table in order of newest to oldest and limit to 5
     const page = url.searchParams.get('page')?parseInt(url.searchParams.get('page')!,10):1
     const per_page = url.searchParams.get('per_page')?parseInt(url.searchParams.get('per_page')!,10):5
+    //cast to string to bool
+    const get_length = url.searchParams.get('length')?.toLowerCase() === 'true'
     const start = page*per_page-per_page
     const end = page*per_page-1
     const { data, error} = await supabase
@@ -21,5 +23,14 @@ export const GET: RequestHandler = async ({url}) => {
     data.forEach((blog: Blog) => {
         blog.created_at = new Date(blog.created_at).toLocaleDateString()
     })
+    if(get_length){
+        const { count: length, error: length_error } = await supabase
+            .from('blogs')
+            .select('*', { head: true, count: 'exact' })
+        if (length_error) {
+            return json({"data":"","error": length_error})
+        }
+        return json({data, length})
+    }
     return json({data, error})
 }
